cbbf91fd04649b8be2688c0b9c3cba3036299e9f
Mostafa kashif <mostafa_kashif@hotmail.com>
2018-10-28 08:05:56 +0200

    target commit

---
+++ evolvice/demo/target/generated-sources/annotations/com/evolvice/task/evolvice/entities/Car_.java
@@ -0,0 +1,17 @@
+package com.evolvice.task.evolvice.entities;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-28T02:11:51")
+@StaticMetamodel(Car.class)
+public class Car_ { 
+
+    public static volatile SingularAttribute<Car, String> yearOfProduction;
+    public static volatile SingularAttribute<Car, String> model;
+    public static volatile SingularAttribute<Car, String> category;
+    public static volatile SingularAttribute<Car, String> brand;
+    public static volatile SingularAttribute<Car, Integer> carId;
+
+}
